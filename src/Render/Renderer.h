4b4e78f157ed05219b548d284fd78a2ba15aa944
Nick-Fanelli <nickfanelli5@gmail.com>
2021-05-24 19:39:17 -0400

    perf: remove duplicate vertices when loading models

--- src/Render/Renderer.h
+++ src/Render/Renderer.h
@@ -26,6 +26,14 @@ namespace HarmonyEngine {
         Vertex(const glm::vec3& position) : Position(position), Normal({0, 0, 0}), Color({1, 1, 1, 1}), TextureCoord({0, 0}), TextureID(0) {}
 
         Vertex(const glm::vec3& position, const glm::vec3& normal) : Position(position), Normal(normal), Color({1, 1, 1, 1}), TextureCoord({0, 0}), TextureID(0) {}
+
+        bool operator==(const Vertex& v) {
+            return (v.Position == Position) && (v.Normal == Normal) && (v.Color == Color) && (v.TextureCoord == TextureCoord) && (v.TextureID == TextureID);
+        }
+
+        bool Equals(const glm::vec3& position, const glm::vec3& normal) {
+            return (Position == position) && (Normal == normal);
+        } 
     };
 
     struct Mesh {
@@ -69,7 +77,7 @@ namespace HarmonyEngine {
             s_Shader.Bind();
             s_Shader.AddUniformMat4("uViewProjectionMatrix", s_Camera->GetProjectViewMatrix());
             s_Shader.AddUniformIntArray("uTextures", s_Batch.TextureIndex, s_Batch.Textures);
-            s_Shader.AddUniformVec3("uLightPosition", {0, 20, 20});
+            s_Shader.AddUniformVec3("uLightPosition", {0, 20, -20});
 
             for(int i = 0; i < s_Batch.TextureIndex; i++) {
                 glActiveTexture(GL_TEXTURE0 + i);
@@ -235,8 +243,93 @@ namespace HarmonyEngine {
         // Utility Functions
 
         void LoadOBJFile(const char* filepath, Mesh* mesh) {
-            
-            
+            std::vector<uint32_t> vertexIndices, uvIndices, normalIndices;
+            std::vector<glm::vec3> tempVertices;
+            std::vector<glm::vec2> tempUvs;
+            std::vector<glm::vec3> tempNormals;
+
+            FILE* file = fopen(filepath, "r");
+            if(file == NULL) {
+                Log::Error("Could not find and load obj file!");
+                return;
+            }
+
+            while(true) {
+                char lineHeader[128];
+
+                int res = fscanf(file, "%s", lineHeader);
+                if(res == EOF)
+                    break;
+
+                if(strcmp(lineHeader, "v") == 0) {
+                    glm::vec3 vertex;
+                    fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
+                    tempVertices.push_back(vertex);
+                } else if(strcmp(lineHeader, "vt") == 0) {
+                    glm::vec2 uv;
+                    fscanf(file, "%f %f\n", &uv.x, &uv.y);
+                    tempUvs.push_back(uv);
+                } else if(strcmp(lineHeader, "vn") == 0) {
+                    glm::vec3 normal;
+                    fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
+                    tempNormals.push_back(normal);
+                } else if(strcmp(lineHeader, "f") == 0) {
+                    uint32_t vertexIndex[3], uvIndex[3], normalIndex[3];
+                    int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2] );
+                    if(matches != 9) {
+                        Log::Error("Could not load obj file format!");
+                        return;
+                    }
+
+                    vertexIndices.push_back(vertexIndex[0]);
+                    vertexIndices.push_back(vertexIndex[1]);
+                    vertexIndices.push_back(vertexIndex[2]);
+                    uvIndices    .push_back(uvIndex[0]);
+                    uvIndices    .push_back(uvIndex[1]);
+                    uvIndices    .push_back(uvIndex[2]);
+                    normalIndices.push_back(normalIndex[0]);
+                    normalIndices.push_back(normalIndex[1]);
+                    normalIndices.push_back(normalIndex[2]);
+                }
+            }
+
+            // vertices.push_back(Vertex({1, 1, 1}, {1, 1, 1}));
+            // Vertex vertex = Vertex({1, 1, 1}, {1, 1, 1});
+
+            // for(auto& v : vertices) {
+            //     if(v == vertex) 
+            //         Log::Info("Hey");
+            // }
+
+            uint32_t duplicateOffset = 0;
+
+            bool isDuplicate = false;
+            uint32_t duplicatePosition = 0;
+
+            for(uint32_t i = 0; i < vertexIndices.size(); i++) {
+                uint32_t vertexIndex = vertexIndices[i];
+                uint32_t normalIndex = normalIndices[i];
+
+                glm::vec3 vertexPosition = tempVertices[vertexIndex - 1];
+                glm::vec3 vertexNormal = tempNormals[normalIndex - 1];
+
+                for(uint32_t i = 0; i < mesh->Vertices.size(); i++) {
+                    if(mesh->Vertices[i].Equals(vertexPosition, vertexNormal)) {
+                        duplicateOffset++;
+                        duplicatePosition = i;
+                        isDuplicate = true; 
+                        break;
+                    }
+                }
+
+                if(!isDuplicate){
+                    mesh->Vertices.push_back(Vertex(vertexPosition, vertexNormal));
+                    mesh->Indices.push_back(i - duplicateOffset);
+                } else {
+                    mesh->Indices.push_back(duplicatePosition);
+                    isDuplicate = false;
+                }
+            }
         }
     }
 
