3fc7ed4e4a5e7218b73940cbd4acfaedca32ba16
Nick-Fanelli <nickfanelli5@gmail.com>
2021-05-24 18:05:49 -0400

    feat: format logging

--- src/Core/Log.h
+++ src/Core/Log.h
@@ -33,10 +33,32 @@ namespace HarmonyEngine::Log {
             std::cout << ANSI_BLUE_BOLD << "Harmony Engine - [INFO]: " << ANSI_BLUE << message << ANSI_RESET << std::endl;
     }
 
+    void FormatInfo(const char* format, ...) {
+        va_list args;
+        va_start(args, format);
+
+        if(s_LogLevel >= LogLevelInfo) {
+            printf("%HarmonyEngine - [INFO]:%s ", ANSI_BLUE_BOLD, ANSI_BLUE);
+            vfprintf(stdout, format, args);
+            printf("%s\n", ANSI_RESET);
+        }
+    }
+
     template<typename T>
     void Success(const T& message) {
         if(s_LogLevel >= LogLevelInfo)
             std::cout << ANSI_GREEN_BOLD << "Harmony Engine - [SUCCESS]: " << ANSI_GREEN << message << ANSI_RESET << std::endl;
+    }
+
+    void FormatSuccess(const char* format, ...) {
+        va_list args;
+        va_start(args, format);
+
+        if(s_LogLevel >= LogLevelInfo) {
+            printf("%Harmony Engine - [SUCCESS]:%s ", ANSI_GREEN_BOLD, ANSI_GREEN);
+            vfprintf(stdout, format, args);
+            printf("%s\n", ANSI_RESET);
+        }
     }
 
     template<typename T>
@@ -45,10 +67,36 @@ namespace HarmonyEngine::Log {
             std::cout << ANSI_YELLOW_BOLD << "Harmony Engine - [WARNING]: " << ANSI_YELLOW << message << ANSI_RESET << std::endl;
     }
 
+    void FormatWarn(const char* format, ...) {
+        va_list args;
+        va_start(args, format);
+
+        if(s_LogLevel >= LogLevelWarn) {
+            printf("%sHarmony Engine - [WARNING]:%s ", ANSI_YELLOW_BOLD, ANSI_YELLOW);
+            vfprintf(stdout, format, args);
+            printf("%s\n", ANSI_RESET);
+        }
+
+        va_end(args);
+    }
+
     template<typename T>
     void Error(const T& message) {
         if(s_LogLevel >= LogLevelError)
             std::cout << ANSI_RED_BOLD << "Harmony Engine - [ERROR]: " << ANSI_RED << message << ANSI_RESET << std::endl;
+    }
+
+    void FormatError(const char* format, ...) {
+        va_list args;
+        va_start(args, format);
+        
+        if(s_LogLevel >= LogLevelError) {
+            printf("%sHarmony Engine - [ERROR]:%s ", ANSI_RED_BOLD, ANSI_RED);
+            vfprintf(stdout, format, args);
+            printf("%s\n", ANSI_RESET);
+        }
+
+        va_end(args);
     }
 
 }
