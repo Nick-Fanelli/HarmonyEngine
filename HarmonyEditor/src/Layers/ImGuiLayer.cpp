28005589d342291902435ddf2cb758c991508152
Nick-Fanelli <nickfanelli5@gmail.com>
2021-06-13 08:42:24 -0400

    feat: basic implementation of ImGUILayer

---
+++ HarmonyEditor/src/Layers/ImGuiLayer.cpp
@@ -0,0 +1,13 @@
+#include "ImGuiLayer.h"
+
+void ImGuiLayer::OnCreate() {
+
+}
+
+void ImGuiLayer::OnUpdate() {
+
+}
+
+void ImGuiLayer::OnDestroy() {
+    
+}
---
+++ HarmonyEditor/src/Layers/ImGuiLayer.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <harmonypch.h>
+
+using namespace HarmonyEngine;
+
+namespace ImGuiLayer {
+
+    void OnCreate();
+    void OnUpdate();
+    void OnDestroy();
+
+};
