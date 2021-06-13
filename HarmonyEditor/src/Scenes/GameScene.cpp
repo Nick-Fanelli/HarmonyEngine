cb730004ff86a8c10dc8cac3b6d87b81fc6c60c3
Nick-Fanelli <nickfanelli5@gmail.com>
2021-06-12 23:33:21 -0400

    refactor: break GameScene apart into cpp and header file

---
+++ HarmonyEditor/src/Scenes/GameScene.cpp
@@ -0,0 +1,64 @@
+#include "GameScene.h"
+
+static PerspectiveCamera s_Camera;
+static AssetHandle<Mesh> s_Mesh;
+
+void GameScene::OnCreate() {
+    s_Camera = PerspectiveCamera();
+    
+    // Renderer2D::OnCreate(&m_Camera);
+    Renderer::OnCreate(&s_Camera);
+
+    s_Mesh = AssetManager::QueueMesh("assets/objects/stall.obj");
+
+    AssetManager::CreateAll();
+}
+
+void GameScene::Update(float deltaTime) {
+
+    constexpr float moveSpeed = 3.5f;
+    constexpr float mouseSensitivity = 4.5f;
+
+    if(Input::IsKey(HARMONY_KEY_W)) {
+        s_Camera.Move(glm::vec3(0.0f, 0.0f, moveSpeed * deltaTime));
+    }
+
+    if(Input::IsKey(HARMONY_KEY_S)) {
+        s_Camera.Move(glm::vec3(0.0f, 0.0f, -moveSpeed * deltaTime));
+    }
+
+    if(Input::IsKey(HARMONY_KEY_A)) {
+        s_Camera.Move(glm::normalize(glm::cross(s_Camera.GetCameraFront(), s_Camera.GetCameraUp())) * moveSpeed * deltaTime);
+    }
+
+    if(Input::IsKey(HARMONY_KEY_D)) {
+        s_Camera.Move(glm::normalize(glm::cross(s_Camera.GetCameraFront(), s_Camera.GetCameraUp())) * -moveSpeed * deltaTime);
+    }
+
+    if(Input::IsKey(HARMONY_KEY_SPACE)) {
+        s_Camera.Move(moveSpeed * deltaTime * s_Camera.GetCameraUp());
+    }
+
+        if(Input::IsKey(HARMONY_KEY_LEFT_SHIFT)) {
+        s_Camera.Move(-moveSpeed * deltaTime * s_Camera.GetCameraUp());
+    }
+
+    const glm::vec2 zero = {0, 0};
+
+    if(Input::GetDeltaMousePosition() != zero) {
+        s_Camera.Rotate(Input::GetDeltaMousePosition() * mouseSensitivity * deltaTime); 
+    }
+
+    Renderer::StartBatch();
+
+    Renderer::DrawMesh(s_Mesh);
+
+    Renderer::EndBatch();
+}
+
+void GameScene::OnDestroy() {
+    Renderer::OnDestroy();
+    // Renderer2D::OnDestroy();
+    m_Registry.clear<>();
+    AssetManager::DestroyAll();
+}
--- HarmonyEditor/src/Scenes/GameScene.h
+++ HarmonyEditor/src/Scenes/GameScene.h
@@ -19,70 +19,8 @@
 using namespace HarmonyEngine;
 
 class GameScene : public Scene {
-
-    PerspectiveCamera m_Camera;
-    AssetHandle<Mesh> m_Mesh;
-
 public:
-
-    void OnCreate() override {
-        m_Camera = PerspectiveCamera();
-        
-        // Renderer2D::OnCreate(&m_Camera);
-        Renderer::OnCreate(&m_Camera);
-
-        m_Mesh = AssetManager::QueueMesh("assets/objects/stall.obj");
-
-        AssetManager::CreateAll();
-    }
-
-    void Update(float deltaTime) override {
-
-        constexpr float moveSpeed = 3.5f;
-        constexpr float mouseSensitivity = 4.5f;
-
-        if(Input::IsKey(HARMONY_KEY_W)) {
-            m_Camera.Move(glm::vec3(0.0f, 0.0f, moveSpeed * deltaTime));
-        }
-
-        if(Input::IsKey(HARMONY_KEY_S)) {
-            m_Camera.Move(glm::vec3(0.0f, 0.0f, -moveSpeed * deltaTime));
-        }
-
-        if(Input::IsKey(HARMONY_KEY_A)) {
-            m_Camera.Move(glm::normalize(glm::cross(m_Camera.GetCameraFront(), m_Camera.GetCameraUp())) * moveSpeed * deltaTime);
-        }
-
-        if(Input::IsKey(HARMONY_KEY_D)) {
-            m_Camera.Move(glm::normalize(glm::cross(m_Camera.GetCameraFront(), m_Camera.GetCameraUp())) * -moveSpeed * deltaTime);
-        }
-
-        if(Input::IsKey(HARMONY_KEY_SPACE)) {
-            m_Camera.Move(moveSpeed * deltaTime * m_Camera.GetCameraUp());
-        }
-
-         if(Input::IsKey(HARMONY_KEY_LEFT_SHIFT)) {
-            m_Camera.Move(-moveSpeed * deltaTime * m_Camera.GetCameraUp());
-        }
-
-        const glm::vec2 zero = {0, 0};
-
-        if(Input::GetDeltaMousePosition() != zero) {
-            m_Camera.Rotate(Input::GetDeltaMousePosition() * mouseSensitivity * deltaTime); 
-        }
-
-        Renderer::StartBatch();
-
-        Renderer::DrawMesh(m_Mesh);
-
-        Renderer::EndBatch();
-    }
-
-    void OnDestroy() override {
-        Renderer::OnDestroy();
-        // Renderer2D::OnDestroy();
-        m_Registry.clear<>();
-        AssetManager::DestroyAll();
-    }
-
+    void OnCreate() override;
+    void Update(float deltaTime) override;
+    void OnDestroy() override;
 };
